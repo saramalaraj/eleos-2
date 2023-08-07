9a375aa91c4a13da314ee713bd691ddeada85799
sara <saraalaraj28@gmail.com>
2023-08-07 16:43:12 +0400

    Initial Commit

---
+++ me/meApp.swift
@@ -0,0 +1,17 @@
+//
+//  meApp.swift
+//  me
+//
+//  Created by sara alaraj on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
